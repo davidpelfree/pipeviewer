05afc5e5ed964d9e4d86408ddcc1fe29572e2295
ivarch <ivarch@e2fa8b35-d329-0410-904f-67ec9c077cba>
2003-01-29 21:19:10 +0000

    *** empty log message ***

--- autoconf/make/link.mk
+++ autoconf/make/link.mk
@@ -10,4 +10,7 @@ intlobjs = @INTLOBJS@
 $(package): src/main.o $(intldeps) $(intlobjs) $(objgetopt)
 	$(CC) $(FINALFLAGS) -o $@ src/main.o $(intldeps) $(intlobjs) $(objgetopt)
 
+$(package)-static: src/main.o $(intldeps) $(intlobjs) $(objgetopt)
+	$(CC) -static -o $@ src/main.o $(intldeps) $(intlobjs) $(objgetopt) $(FINALFLAGS)
+
 # EOF $Id$
