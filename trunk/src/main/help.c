1eaaa1d2191845a9f0572c092bde32cf881b1c65
ivarch <ivarch@e2fa8b35-d329-0410-904f-67ec9c077cba>
2004-01-14 19:11:28 +0000

    *** empty log message ***

--- trunk/src/main/help.c
+++ trunk/src/main/help.c
@@ -1,7 +1,7 @@
 /*
  * Output command-line help to stdout.
  *
- * Copyright 2002 Andrew Wood, distributed under the Artistic License.
+ * Copyright 2004 Andrew Wood, distributed under the Artistic License.
  */
 
 #ifdef HAVE_CONFIG_H
--- trunk/src/main/options.c
+++ trunk/src/main/options.c
@@ -1,7 +1,7 @@
 /*
  * Parse command-line options.
  *
- * Copyright 2002 Andrew Wood, distributed under the Artistic License.
+ * Copyright 2004 Andrew Wood, distributed under the Artistic License.
  */
 
 #ifdef HAVE_CONFIG_H
