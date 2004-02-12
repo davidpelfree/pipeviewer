f6ceeaaff317da46f63c4a3f4caeead1d8b0c8dc
ivarch <ivarch@e2fa8b35-d329-0410-904f-67ec9c077cba>
2004-02-12 20:45:33 +0000

    *** empty log message ***

--- src/main/version.c
+++ src/main/version.c
@@ -16,24 +16,26 @@
  */
 void display_version(void)
 {
-	printf(_("%s %s - Copyright(C) %s %s"), /* RATS: ignore */
-	  PROGRAM_NAME, VERSION,
-	  COPYRIGHT_YEAR, COPYRIGHT_HOLDER);
+	printf(_("%s %s - Copyright(C) %s %s"),	/* RATS: ignore */
+	       PROGRAM_NAME, VERSION, COPYRIGHT_YEAR, COPYRIGHT_HOLDER);
 	printf("\n\n");
-	printf(_("Web site: %s"), /* RATS: ignore */
-	  PROJECT_HOMEPAGE);
+	printf(_("Web site: %s"),	    /* RATS: ignore */
+	       PROJECT_HOMEPAGE);
 	printf("\n\n");
-	printf("%s", _("This program is free software, and is being distributed "
-	  "under the\nterms of the Artistic License."));
+	printf("%s",
+	       _("This program is free software, and is being distributed "
+		 "under the\nterms of the Artistic License."));
 	printf("\n\n");
-	printf("%s", _(
-"This program is distributed in the hope that it will be useful,\n"
-"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."));
+	printf("%s",
+	       _
+	       ("This program is distributed in the hope that it will be useful,\n"
+		"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+		"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."));
 	printf("\n\n");
-	printf(/* RATS: ignore */
-	  _("For more information, please run `%s --license'."),
-          PROGRAM_NAME);
+	printf(				    /* RATS: ignore */
+		      _
+		      ("For more information, please run `%s --license'."),
+		      PROGRAM_NAME);
 	printf("\n");
 }
 
